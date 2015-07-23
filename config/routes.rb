4ca7ab0ee152887f96f564c3a56a51e228eef8f6
Josh Bonnell <thesmokinghyena@gmail.com>
2015-07-23 11:08:55 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
   get 'signup'  => 'users#new'
+  resources :users #Handles CRUD ops for user 
 
 end
